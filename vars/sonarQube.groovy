cfa624df5da0d3076f234c9c3e7db6dcf3b2f7b9
swetha2310 <89144008+swetha2310@users.noreply.github.com>
2023-03-02 19:51:05 +0530

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -1,5 +1,4 @@
 def sonarAnalysis(String Project_Name, String Sonar_URL, String Sonar_login){
-      sh "mvn clean install"
       sh "mvn sonar:sonar \
       -Dsonar.projectName=${Project_Name}  \
       -Dsonar.host.url=${Sonar_URL}  \
