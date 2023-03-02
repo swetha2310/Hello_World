29e4020672e3af707affbeb342bb72765640d91f
swetha2310 <89144008+swetha2310@users.noreply.github.com>
2023-03-02 20:06:47 +0530

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -1,7 +1,7 @@
 def sonarAnalysis(String Project_Name, String Sonar_URL, String Sonar_login){
-      echo ${Project_Name}
-      echo ${Sonar_URL}
-      echo ${Sonar_login}
+      sh echo ${Project_Name}
+      sh echo ${Sonar_URL}
+      sh echo ${Sonar_login}
       sh "mvn sonar:sonar \
       -Dsonar.projectName=${Project_Name}  \
       -Dsonar.host.url=${Sonar_URL}  \
