7e0ed73fb9c947338e947e6ae5537991ed68b4d2
yoshithac8 <78433771+yoshithac8@users.noreply.github.com>
2023-03-02 18:16:41 +0530

    Create sonarQube.groovy

---
+++ vars/sonarQube.groovy
@@ -0,0 +1,7 @@
+def sonarAnalysis(String Project_Name, String Sonar_URL, String Sonar_login){
+      sh "mvn clean install"
+      sh "mvn sonar:sonar \
+      -Dsonar.projectName=${Project_Name}  \
+      -Dsonar.host.url=${Sonar_URL}  \
+      -Dsonar.login=${Sonar_login}"
+}
